9c44805b163a23f25fb80e8d9a36713e20810998
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2021-09-03 10:26:53 +0200

    Fix #2 by adding comments

--- downscaling/utils.py
+++ downscaling/utils.py
@@ -8,24 +8,33 @@ logger = logging.getLogger(__name__)
 def validate_input_data(generation=None, pop_density=None, population=None):
 
     """
+
     Parameters
     ----------
     generation : IamDataFrame, required
-        Includes the heat generation per technology/source. The default is None.
+        Includes the heat generation by technology/source.
+        So far, it is necessary to include values of one scenario here.
+        This will be updated in further extensions.
+        The default is None.
     pop_density : IamDataFrame, required
-        Includes the population density per region. The default is None.
-    population : IamDataFrame, optional
-        Includes the (total) population per region. The default is None.
+        Includes the population density of the regions (areas to be downscaled).
+        The scenario should be the same as the one of the generation parameter.
+        The default is None.
+    population : IamDataFrame, required
+        Includes the population per region.
+        The scenario should be the same as the one of the 'generation' parameter.
+        The default is None.
 
     Returns
     -------
-    _ret : binary (True/False)
-        True if the input data is in appropiate format, else False.
+    check : binary (True/False)
+        This variable is set to 'True' if all the input data is in the appropiate format.
+        Otherwise, it is set to 'False'.
 
     """
 
     _string = []
-    _ret = True
+    check = True
 
     if not isinstance(generation, py.IamDataFrame):
         _string.append("Generation")
@@ -43,7 +52,7 @@ def validate_input_data(generation=None, pop_density=None, population=None):
             else "{} input data are not in the IamDataFrame format"
         )
         logger.warning(msg.format(n, _string))
-        _ret = False
+        check = False
     else:
         logger.info("All input data is in the IamDataFrame format")
 
@@ -62,25 +71,36 @@ def validate_input_data(generation=None, pop_density=None, population=None):
                 else "Scenarios {} do not have complete input data set"
             )
             logger.warning(msg.format(_string))
-            _ret = False
+            check = False
 
-    return _ret
+    return check
 
 
 def initialization(technologies=None, requirements=None):
 
     """
+
     Parameters
     ----------
     technologies : list, required
-        A list of all heat generation technologies/sources. The default is None.
+        A list of heat generation technologies/sources.
+        The default is None.
     requirements : dict, required
-        A dictionary including the requirements of heat generation technologies/sources. The default is None.
+        Includes the heat network infrastructure requirements of the different
+        heat generation technologies. This dictionary should include a specific
+        value for each technology/source. If not, the corresponding value is
+        set to zero for the technology/source.
+        Note that the requirements correspond to the required local population
+        density in the current version of the script.
+        The default is None.
 
     Returns
     -------
-    requirements : dict
-        An updated dictionary including all the requirements of heat generation technologies/sources.
+    full_req : dict
+        Includes a complete (full) dictionary related to all technology/source
+        specific heat network infrastructure requirements.
+        As mentioned, unspecified requirements are set to zero for the
+        corresponding technology/source.
 
     """
 
@@ -88,31 +108,41 @@ def initialization(technologies=None, requirements=None):
         if not _t in requirements.keys():
             print(f"No requirements defined for {_t}! (is set to 0)")
             requirements[_t] = 0
-    requirements = dict(sorted(requirements.items(), key=lambda x: x[1], reverse=True))
-    return requirements
+    full_req = dict(sorted(requirements.items(), key=lambda x: x[1], reverse=True))
+
+    return full_req
 
 
 def pop_based_downscaling(generation=None, population=None):
 
     """
+
     Parameters
     ----------
     generation : IamDataFrame, required
-        Includes the heat generation by technology/source. The default is None.
+        Includes the heat generation by technology/source.
+        So far, it is necessary to include values of one scenario here.
+        This will be updated in further extensions.
+        The default is None.
     population : IamDataFrame, required
-        Includes the total population. The default is None.
+        Includes the population per region.
+        The scenario should be the same as the one of the 'generation' parameter.
+        The default is None.
 
     Returns
     -------
     demand : dict
-        Total heat demand per scenario and region.
+        Includes the population-based downscaled heat demand per region
+        (and scenario). Scenario, as part of the dictionary
+        key is used for further extensions of the script: for example
+        processing multiple scenarios at the same time.
 
     """
 
     total_generation = dict()
     demand = dict()
-    _scenarios = generation.scenario
-    for _sce in _scenarios:
+    scenarios = generation.scenario
+    for _sce in scenarios:
         total_generation = generation.filter(scenario=_sce).data["value"].sum()
         total_population = population.filter(scenario=_sce).data["value"].sum()
         for _r in population.filter(scenario=_sce).region:
@@ -123,16 +153,19 @@ def pop_based_downscaling(generation=None, population=None):
                     / total_population
                 )[0]
             )
+
     return demand
 
 
 def iamdf_to_dict(df=None, keys=None):
 
     """
+
     Parameters
     ----------
     df : IamDataFrame, required
-        Includes the data as an IamDataFrame. The default is none.
+        Includes the data in the IAMC format that is tranformed to a dict.
+        The default is none.
     keys : list, required
         A list containing the columns of the IamDataFrame used as key.
         The order of elements within the list needs to be same as the columns
@@ -145,6 +178,7 @@ def iamdf_to_dict(df=None, keys=None):
         A dictionary with selected keys and corresponding values.
 
     """
+
     _dict = dict()
     _pandas_df = df.data
     for index, row in _pandas_df.iterrows():
@@ -173,30 +207,45 @@ def iamdf_to_dict(df=None, keys=None):
 def sequential_algorithm(
     generation=None, demand=None, requirements=None, potential=None, scenario=None
 ):
+    
     """
+    
     Parameters
     ----------
     generation : dict, required
-        Heat generation by technology/source with the following keys: scenario, variable. The default is None.
+        A dictionary including the heat generation by technology/source with 
+        scenario and variable as keys. The scenario-related key is already 
+        implemented for further extensions of the script. 
+        The default is None. 
     demand : dict, required
-        Total heat demand per scenario and region. The default is None.
+        A dictionary including the (downscaled) heat demand on the region 
+        level with scenario and region as keys. 
+        The scenario-related key is already 
+        implemented for further extensions of the script. 
+        The default is None. 
     requirements : dict, required
-        Requirements of heat network infrastructure at the local level.
+        Requirements of heat network infrastructure at the local level. The key
+        of the dictionary is the heat generation technology/source. 
         The default is None.
     potential : dict, required
-        Potential of heat network infrastructure at the local level.
+        Potential of heat network infrastructure at the local level. The key
+        of the dictionary is the region. 
         The default is None.
     scenario : string, required
+        Sets the scenario. This parameter is already considered to help further
+        developments of the script related to multiple scenario analyses. 
         The default is None.
 
     Returns
     -------
-    _quantity : dict
-        Heat generation by source/technology per scenario and region.
+    quantity : dict
+        The heat generation by technology/source at the local level.
+        The key of the dict is a tuple including scenario, variable, 
+        and region (in this order).
 
     """
 
-    _quantity = dict()
+    quantity = dict()
     for _k_req in requirements.keys():
         _list = []
         _load = 0
@@ -210,29 +259,42 @@ def sequential_algorithm(
                 _load += demand[scenario, _r]
 
         for _l in _list:
-            _quantity[scenario, _k_req, _l] = (
+            quantity[scenario, _k_req, _l] = (
                 demand[scenario, _l] / _load
             ) * generation[scenario, _k_req]
-            demand[scenario, _l] -= _quantity[scenario, _k_req, _l]
-    return _quantity
+            demand[scenario, _l] -= quantity[scenario, _k_req, _l]
+    return quantity
 
 
-def dict_to_iamdf(dictionary=None, col_name=None):
+def dict_to_df(dictionary=None, col_name=None):
 
     """
+
     Parameters
     ----------
     dictionary : dict, required
-        Includes the data that is transformed to an IamDataFrame. The key of the dictionary should include the following IAMC format columns: scenario, variable, region. The default is None.
-    columns : dict, required
-        Includes additonal information that is used to create a complete IamDataFrame (e.g., model, unit, etc.). The default is None.
+        Includes a dictionary that is tranformed to the IAMC format and
+        IamDataFrame. It is required that the key tuple of the dictionary
+        includes the information of the 'scenario', 'variable', and 'region'
+        column (in this order).
+        The default is None.
+    col_name : list, optional
+        Includes column names that are used for the IAMC format. If this
+        parameter is not passed to the function, the default description,
+        as described above in the 'dictionary' description, is used.
+        The default is None.
 
     Returns
     -------
-    df : IamDataFrame
+    df : DataFrame
 
     """
 
     df = df = pd.DataFrame(dictionary, index=[0]).T.reset_index()
-    df.columns = col_name
+
+    if col_name == None:
+        df.columns = ["Scenario", "Variable", "Region", "Value"]
+    else:
+        df.columns = col_name
+
     return df
